4782bac1407f6e2f237639ef89f62be75f269644
Harshaneel Gokhale <harshaneelhg@gmail.com>
2015-06-04 17:25:49 -0700

    Update: disease-chem-preprocessing
    - Added Gene processing
    - Added Disease-Gene data

--- Thesis/Programs/disease-chem_preprocess.py
+++ Thesis/Programs/disease-chem_preprocess.py
@@ -5,6 +5,9 @@ import scipy.io
 import csv
 from sklearn.preprocessing import normalize
 
+
+###################################### Function Definitions ########################################
+
 def rwr_algo(q, c, W):
 	"""
 		Input:
@@ -38,61 +41,119 @@ def rwr_algo(q, c, W):
 	print 'Reached to the convergence in %s iterations.'%i
 	return r1
 
-def preprocess_disease_chem(filename):
+def preprocess_disease_chem():
 	"""
 		Input:
-			filename: Name of the CSV file containing disease chemical association.
+			filename: Name of the CSV file containing disease-chemical association
+			and disease-gene association.
 		Output:
-			Sparse matrix of disease_chem interaction.
+			n : Size of adjecency matrix.
+			A : Adjecency matrix of disease-chemical-gene association.
+			diseases_dict : Dictionary of disease names and their DOIDs.
+			chem_dict : Dictionary of chemical names and their CHIDs.
+			disease_id : Dictionary of column/row numbers of diseases and their DOID.
+			chem_id : Dictionary of column/row numbers of chemicals and their CHID.
+			gene_id : Dictionary of column/row numbers of genes and gene names.
 
-		Example:
-			Input-
-			ID	Name Chem
-			1	abc1 chm_1
-			2	abc2 chm_2
-			3	abc3 chm_3
-			:	:	 :	
-			n 	abcn chm_n
+		Matrix structure for Disease-Chemical-Genes association:
 
-			Output-
-			Sparse matrix:
-			(0,0) chm_1
-			(0,1) chm_2
-			:	  :
+				D 		C 		G 
+			-------------------------
+		    |	 	|		|		|
+		D	|	0	|		|		|
+			|		|		|		|
+			-------------------------
+		 	|		|		|		|
+		C	|		|	0	|	0	|
+			|		|		|		|
+			-------------------------
+		 	|		|		|		|
+		G	|		|	0	|	0	|
+			|		|		|		|
+			-------------------------
 	"""
-	# Read Dataset
-	data = pd.read_csv('../Data/disease_chemical_associations.csv')
-	# Separate required columns
-	doid= data['DOID']
-	diseases = data['Disease_Name']
-	chid = data['Chemical_ID']
-	chemicals = data['Chemical_Name']
+	
+	# Read Dataset.
+	print 'reading dataset...'
+	data_dc = pd.read_csv('../Data/disease_chemical_associations.csv')
+	data_dg = pd.read_csv('../Data/disease_gene_associations.csv')
+	
+	# Separate required columns.
 
-	# Find Unique Diseases and Chemicals
+	doid_dc= data_dc['DOID'].tolist()
+	diseases_dc = data_dc['Disease_Name'].tolist()
+	chid = data_dc['Chemical_ID'].tolist()
+	chemicals = data_dc['Chemical_Name'].tolist()
 
-	diseases_unique = np.unique(doid).tolist()
-	chemicals_unique = np.unique(chid).tolist()
+	doid_dg= data_dg['DOID'].tolist()
+	diseases_dg = data_dg['Disease_Name'].tolist()
+	genes = data_dg['Gene_Symbol'].tolist()
+	
+	# Find Unique Diseases,
+	# Create disease-name dictionary,
+	# Create disease-id dictionary.
 
+	diseases_unique = np.unique(doid_dc+doid_dg).tolist()
 	n_diseases = len(diseases_unique)
+	temp_diseases = diseases_dc+diseases_dg
+	temp_doid = doid_dc+doid_dg
+	diseases_dict = {}
+	disease_id = {}
+	for d in diseases_unique:
+		i = temp_doid.index(d)
+		diseases_dict[temp_doid[i]] = temp_diseases[i] 
+		disease_id[diseases_unique.index(d)] = d
+
+	# Find Unique Chemicals,
+	# Create chemical-name dictionary,
+	# Create chemical-id dictionary.
+
+	chemicals_unique = np.unique(chid).tolist()
 	n_chemicals = len(chemicals_unique)
-	n = n_diseases+n_chemicals
+	chem_dict= {}
+	chem_id = {}
+	for c in chemicals_unique:
+		i = chid.index(c)
+		chem_dict[chid[i]]=chemicals[i]
+		chem_id[chemicals_unique.index(c)+n_diseases] = c
 
-	dc = scipy.sparse.lil_matrix((n,n),dtype=float)
+	# Find Unique Genes,
+	# Create gene-id dictionary.
 
-	for i in range(0, len(doid)):
-		id1 = diseases_unique.index(doid[i])
-		id2 = chemicals_unique.index(chid[i])
-		dc[id1, n_diseases+id2] = 1.0
-		dc[n_diseases+id2, id1] = 1.0
+	genes_unique = np.unique(genes).tolist()
+	n_genes = len(genes_unique)
+	gene_id = {}
+	for g in genes_unique:
+		gene_id[genes_unique.index(g)+n_diseases+n_chemicals] = g
 
-	dc = dc.tocoo()
-	return n, dc
+	n = n_diseases+n_chemicals+n_genes
 
+	# Create sparse matrix of disease-chemical-gene association.
+	print 'Constructing sparse matrix...'
+	dcg = scipy.sparse.lil_matrix((n,n),dtype=float)
 
-n, A = preprocess_disease_chem('temp')
+	for i in range(0, len(doid_dc)):
+		id1 = diseases_unique.index(doid_dc[i])
+		id2 =  n_diseases+chemicals_unique.index(chid[i])
+		dcg[id1, id2] = 1.0
+		dcg[id2, id1] = 1.0
+
+	for i in range(0, len(doid_dg)):
+		id1 = diseases_unique.index(doid_dg[i])
+		id2 = n_diseases+n_chemicals+genes_unique.index(genes[i])
+		dcg[id1, id2] = 1.0
+		dcg[id2, id1] = 1.0
+
+	dcg = dcg.tocoo()
+	return n, dcg, diseases_dict, chem_dict, disease_id, chem_id, gene_id
+
+############################################ Main Program ##########################################
+
+print 'Pre-processing data...'
+n, A, diseases_dict, chem_dict, disease_id, chem_id, gene_id = preprocess_disease_chem()
 q = scipy.sparse.lil_matrix((1,n),dtype=float)
 q[0,0]=1.0
-
+print 'Executing query...'
 r1 = rwr_algo(q, 0.9, A)
 print r1
 scipy.io.savemat('ranking.mat',{'ranking_vector':r1})
