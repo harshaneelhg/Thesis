ac67934896796373a2980f2996fecbeb8a27c440
Harshaneel Gokhale <harshaneelhg@gmail.com>
2015-06-09 15:56:42 -0700

    Updated Web app
    - Added results on the display.
    - Modified CSS little bit.
    - Integrated Random Walk code with web app.

--- Thesis/WebApp/index.py
+++ Thesis/WebApp/index.py
@@ -1,10 +1,16 @@
 import flask, flask.views
 import json
 import scipy.io
+import scipy.sparse
+import imp
+import time
 
 app = flask.Flask(__name__)
-
+app.secret_key= 'RandomWalk'
+session = []
 data = scipy.io.loadmat('../Data/preprocessed_dcg_sparse.mat')
+
+r = imp.load_source('rwr_algo', '../Programs/rwr.py')
 
 A = data['adjecency_matrix']
 n = int(data['number_of_nodes'])
@@ -34,14 +40,63 @@ for g in gene_id:
 	genes.append((g,gene_id[g]))
 
 class Home(flask.views.MethodView):
-
+	
 	def get(self):
-		return flask.render_template('index.html', genes = genes, diseases = diseases, chemicals = chemicals)
+		if 'time' in session:
+			session.pop()
+		return flask.render_template('index.html', genes = genes, diseases = diseases, chemicals = chemicals, session= session)
 
 	def post(self):
-		return flask.render_template('index.html', genes = genes, diseases = diseases, chemicals = chemicals)
-		if 'disease-query' in flask.request.form :
+		start = time.time()
+		index = None
+		if 'disease_query' in flask.request.form :
 			index = flask.request.form['disease']
+		elif 'chemical_query' in flask.request.form :
+			index = flask.request.form['chemical']
+		else:
+			index = flask.request.form['gene']
+		q = scipy.sparse.lil_matrix((1,n),dtype=float)
+		q[0,int(index)]=1.0
+		r1 = r.rwr_algo(q, 0.9, A)
+		# Separate ranking vector into diseases, chemicals and genes.
+		r1 = r1.todense()
+		r1 =r1.tolist()[0]
+		d = r1[0:n_diseases]
+		c = r1[n_diseases:n_diseases+n_chemicals]
+		g = r1[n_diseases+n_chemicals:n]
+
+		# Sort Ranking results.
+		sorted_d = [[i[0],i[1]] for i in sorted(enumerate(d), key=lambda x:x[1])]
+		sorted_c = [[i[0],i[1]] for i in sorted(enumerate(c), key=lambda x:x[1])]
+		sorted_g = [[i[0],i[1]] for i in sorted(enumerate(g), key=lambda x:x[1])]
+
+		disease_display=[]
+		last = len(sorted_d)-1
+		for i in range(0,5):
+			disease_display.append([disease_id[str(sorted_d[last-i][0])], diseases_dict[disease_id[str(sorted_d[last-i][0])]], sorted_d[last-i][1]])
+
+		chem_display=[]
+		last = len(sorted_c)-1
+		for i in range(0,5):
+			chem_display.append([chem_id[str(n_diseases+sorted_c[last-i][0])], chem_dict[chem_id[str(n_diseases+sorted_c[last-i][0])]], sorted_c[last-i][1]])
+
+		gene_display=[]
+		last = len(sorted_g)-1
+		for i in range(0,5):
+			gene_display.append([gene_id[str(n_chemicals+n_diseases+sorted_g[last-i][0])], sorted_g[last-i][1]])
+
+		session.append('time')
+
+		return flask.render_template('index.html', 
+									  genes = genes, 
+									  diseases = diseases, 
+									  chemicals = chemicals,
+									  disease_display = disease_display,
+									  chem_display = chem_display,
+									  gene_display = gene_display,
+									  time = time.time()-start,
+									  session = session
+									  )
 
 app.add_url_rule(
 				 '/',
