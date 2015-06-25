f82152ed3451c2253cb8369acaee95cd371d767f
Harshaneel Gokhale <harshaneelhg@gmail.com>
2015-06-24 17:35:50 -0700

    Added code to avoid repeated evaluation of graph.

--- Thesis/Programs/spanning_tree.py
+++ Thesis/Programs/spanning_tree.py
@@ -5,8 +5,11 @@
 import scipy.sparse
 from networkx import minimum_spanning_tree
 from networkx import from_scipy_sparse_matrix
+from networkx.readwrite import json_graph
 from scipy.io import loadmat
 import time
+import json
+import os.path
 
 def find_min_spanning_tree(A):
 	"""
@@ -16,12 +19,27 @@ def find_min_spanning_tree(A):
 			T : Minimum spanning tree.
 			run_time : Total runtime to find minimum spanning tree 
 
-
-
 	"""
+	# Record start time.
 	start = time.time()
-	G = from_scipy_sparse_matrix(A)
+
+	# Check if graph is pre-processed, if yes then don't process it again.
+	if os.path.exists('../Data/dcg_graph.json'):
+		with open('../Data/dcg_graph.json') as data:
+			d = json.load(data)
+			G = json_graph.node_link_graph(d)
+
+	# If graph is not preprocessed then convert it to a Graph and save it to a JSON file.
+	else:
+		G = from_scipy_sparse_matrix(A)
+		data = json_graph.node_link_data(G)
+		with open('../Data/dcg_graph.json', 'w') as outfile:
+			json.dump(data, outfile)
+
+	# Find MST.
 	T = minimum_spanning_tree(G)
+
+	#Record total Runtime
 	run_time = time.time()-start
 	return T, run_time
 
@@ -36,3 +54,5 @@ tree, run_time = find_min_spanning_tree(adj)
 
 # *** USE T.edges TO ACCESS EDGES IN THE RETURNED MINIMUM SPANNING TREE.
 # *** USE T.nodes TO ACCESS NODES IN THE RETURNED MINIMUM SPANNING TREE.
+
+print 'Runtime: %s'%(run_time)
